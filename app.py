eba7ba6af0d8fc22cb152464f7fd0d4521960517
Manasa N <manasanarayanaswami34@gmail.com>
2026-08-08 19:32:10 +0530

    Fix Gmail SMTP connection for Render

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ app.secret_key = os.environ.get(
 # ------------------------------
 
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
 
